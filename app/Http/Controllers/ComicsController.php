69b2841c1777f8288b70cab0e98041569f3ad025
Alexis Gallegos <98421465+galle997@users.noreply.github.com>
2022-04-19 14:09:24 -0600

    se agrega carpeta resource

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Comic;
+use App\Http\Resources\Comic as ComicResource;
 
 class ComicsController extends Controller
 {
     //
-    
+
 }

e455e38d03f123b69cc141452f8e6e5c97259abc
Alexis Gallegos <98421465+galle997@users.noreply.github.com>
2022-04-19 13:28:33 -0600

    agregando controladores y migracion de la tabla comics

---
+++ app/Http/Controllers/ComicsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+
+class ComicsController extends Controller
+{
+    //
+}
---
+++ database/seeders/ComicGames.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicGames extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        \App\Models\ComicGames::factory()->count(50)->create();
+    }
+}
